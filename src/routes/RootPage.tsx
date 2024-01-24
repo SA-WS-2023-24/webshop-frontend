159fbf10ad4741aa34dfbd84b6033f859974367f
= <cle.zander@gmail.com>
2024-01-24 22:18:38 +0100

    removed autogenerated files, added react router and empty root page

---
+++ src/routes/RootPage.tsx
@@ -0,0 +1,10 @@
+
+/**
+ * This page contains only the navigation bar and footer, and allows other
+ * content to be placed within it. It is like a frame for the real content.
+ */
+export default function RootPage() {
+    return (
+        <p>Empty Page</p>
+    );
+}
